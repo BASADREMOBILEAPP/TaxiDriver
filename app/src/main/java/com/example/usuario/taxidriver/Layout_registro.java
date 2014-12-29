4c3f392e53e831cc213a3d59000a7b100f4cfe14
tunime <almaseb_123@hotmail.com>
2014-12-28 21:14:54 -0500

    Creacioon de una nueva actividad y  Editado el manifests

---
+++ app/src/main/java/com/example/usuario/taxidriver/Layout_registro.java
@@ -0,0 +1,16 @@
+package com.example.usuario.taxidriver;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by anderson on 28/12/2014.
+ */
+public class Layout_registro extends Activity {
+
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_registro);
+    }
+}
